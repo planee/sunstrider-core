f81e6e5405b1b41677c683e9f4992f413117d8ce
Kelno <kelnowm@gmail.com>
2018-10-08 20:36:12 +0200

    Restore walk after a CM from a Player

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -10365,6 +10365,10 @@ void Unit::RemoveCharmedBy(Unit* charmer)
         else
             ScheduleAIChange();
     }
+
+    //sun: reset walk state
+    if (GetTypeId() == TYPEID_PLAYER && charmer->GetTypeId() == TYPEID_PLAYER)
+        SetWalk(false);
 }
 
 void Unit::RestoreFaction()
